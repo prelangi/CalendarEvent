c13aa15799f2ec6c5b5fee86a94d75fd591b43ba
Prasanthi Relangi <prasanthi.relangi@gmail.com>
2016-03-19 16:19:06 -0700

    Add methods to delete event; Show alert view when event is added

--- CalendarEvent/CalendarEvent/ViewController.swift
+++ CalendarEvent/CalendarEvent/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
     func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
         
         let event = EKEvent(eventStore: eventStore)
+        var success = false
         event.title = title
         event.startDate = startDate
         event.endDate = endDate
@@ -35,19 +36,57 @@ class ViewController: UIViewController {
         do {
             try eventStore.saveEvent(event, span: .ThisEvent, commit: true)
             savedEventId = event.eventIdentifier
+            success = true
+            
         } catch {
             print("Could not save Event")
         }
         
-    }
+        let message = (success) ? "Event added successfully to calendar" : "Failed to add event to calendar"
+        let alertView = UIAlertView(title: "", message: message, delegate: nil, cancelButtonTitle: nil, otherButtonTitles: "Dismiss")
+        alertView.show()
 
+        
+    }
+    
+    func deleteEvent(eventStore: EKEventStore, eventId: String) {
+        
+        let eventToRemove = eventStore.eventWithIdentifier(eventId)
+        if eventToRemove != nil {
+            do {
+                try eventStore.removeEvent(eventToRemove!, span: .ThisEvent)
+            } catch {
+                print("Could not delete Event")
+            }
+        }
+        
+        
+    }
+    
+    
+    @IBAction func deleteEventFromCalendar(sender: AnyObject) {
+        
+        let eventStore = EKEventStore()
+        if(EKEventStore.authorizationStatusForEntityType(.Event) == EKAuthorizationStatus.Authorized) {
+            deleteEvent(eventStore, eventId: savedEventId)
+            
+        }
+    }
+    
+    
+    
+    
     @IBAction func addEventToCalendar(sender: AnyObject) {
         
         let eventStore = EKEventStore()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss zzz"
+        dateFormatter.timeZone = NSTimeZone.systemTimeZone()
         
-        let startDate = NSDate()
+        let startDate = dateFormatter.dateFromString("2016-05-13 1:30:00 UTC")!
         let endDate = startDate.dateByAddingTimeInterval(60*60)
         
+        //print("Current timezone: "
         if(EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
             eventStore.requestAccessToEntityType(.Event, completion: {
                 (granted:Bool, error: NSError?) -> Void in
@@ -60,6 +99,8 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    
 
 }
 
